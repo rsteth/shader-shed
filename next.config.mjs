6a5c6792ad554e21bc49a5bdbf58ead5bb444ff3
Robin Stethem <robin@stethem.com>
2026-02-04 14:00:23 -0800

    package updates, tweaks
    removed @types/regl (invalid) cleared token updated packages, need to fix turbopack still

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.glsl': ['raw-loader'],
+        '*.vs': ['raw-loader'],
+        '*.fs': ['raw-loader'],
+        '*.vert': ['raw-loader'],
+        '*.frag': ['raw-loader'],
+      },
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
